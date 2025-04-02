4b1d9cab120a47b0829a85c7e78a8e38632f4da2
Akhil Nagpal <akhilnagpal07@gmail.com>
2025-04-02 18:13:39 +0530

    created server

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Server is Running!");
+});
+
+app.get("/jokes", (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: "Why did the backend developer break up?",
+      content: "Because their relationship had too many dependencies!",
+    },
+    {
+      id: 2,
+      title: "What’s a database’s favorite dance move?",
+      content: "The Table Shuffle!",
+    },
+    {
+      id: 3,
+      title: "Why did the API go to therapy?",
+      content: "It had too many unresolved requests!",
+    },
+    {
+      id: 4,
+      title: "How do servers flirt?",
+      content: "They send each other HTTP requests!",
+    },
+    {
+      id: 5,
+      title: "Why don’t programmers like nature?",
+      content: "Too many bugs!",
+    },
+  ];
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening to ${port}`);
+});
